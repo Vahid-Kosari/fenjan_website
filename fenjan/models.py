32a378f9101359c51e40c528fe1a1ce6c03fd2c9
Vahid-Kosari <vahid59m@yahoo.com>
2024-05-28 21:54:31 +0330

    First step to data gathering from web page, keywords were not caught

--- fenjan/models.py
+++ fenjan/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 
-# Create your models here.
+
+# Constants for registration states
+class RegistrationState(models.TextChoices):
+    TRIAL = "Trial", "Trial"
+    REGISTERED = "Registered", "Registered"
+    EXPIRED = "Expired", "Expired"
+
+
+class Customer(AbstractUser):
+    registration_state = models.CharField(
+        max_length=10,
+        choices=RegistrationState.choices,
+        default=RegistrationState.TRIAL,
+    )
+    keywords = models.JSONField(null=False)
+    registration_date = models.DateField(default=timezone.now)
+
+    groups = models.ManyToManyField(
+        "auth.Group",
+        related_name="customer_set",  # Unique related_name for groups
+        blank=True,
+        help_text=(
+            "The groups this user belongs to. A user will get all permissions "
+            "granted to each of their groups."
+        ),
+        verbose_name="groups",
+    )
+    user_permissions = models.ManyToManyField(
+        "auth.Permission",
+        related_name="customer_permission_set",  # Unique related_name for user_permissions
+        blank=True,
+        help_text="Specific permissions for this user.",
+        verbose_name="user permissions",
+    )
+
+    def __str__(self):
+        return self.username
--- fenjan/views.py
+++ fenjan/views.py
@@ -1,7 +1,46 @@
-from django.shortcuts import render
 from django.shortcuts import render, redirect
+
+from .models import Customer, RegistrationState
+from django.utils import timezone
 
 
 def index(request):
-    # base.html is temporarily used to run the project but it will be replaced by standard index and layout ones
     return render(request, "fenjan/index.html")
+
+
+def register(request):
+    if request.method == "POST":
+        name = request.POST["name"]
+        email = request.POST["email"]
+        # Collect keywords from input fields
+        keywords = [request.POST.get(f"keyword{i}", "") for i in range(1, 6)]
+
+        # Split the name into first and last name
+        name_parts = name.split()
+        if len(name_parts) > 1:
+            first_name = name_parts[0]
+            last_name = " ".join(name_parts[1:])
+        else:
+            first_name = name_parts[0]
+            last_name = ""
+
+        # Use the first name as the username if last name is not provided
+        username = (
+            first_name
+            if last_name == ""
+            else f"{first_name}.{last_name}".replace(" ", "")
+        )
+
+        # Create the customer
+        customer = Customer.objects.create_user(
+            username=username,
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            keywords=keywords,
+            registration_state=RegistrationState.TRIAL,
+        )
+
+        return redirect("index")
+
+    return render(request, "register.html")
